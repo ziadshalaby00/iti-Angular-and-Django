5e720b501a8cd25c444513835447c1c58dd0ae2b
ziad shalaby <shalabyziad94@gmail.com>
2025-01-02 22:23:44 -0800

    update

--- lap5&6/myApp/models.py
+++ lap5&6/myApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=128)
+    age = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return self.username
--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits\stdc++.h>
-using namespace std;
-
-int main() {
-    char c;
-    cin >> c;
-    cout << (c == 'z' ? cout << 'a' : char(int(c)+1)) << endl;
-} 
