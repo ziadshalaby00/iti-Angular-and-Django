358cb7aba2ec4a90ede9c8b936bebbed5c9025f2
ziad shalaby <shalabyziad94@gmail.com>
2025-03-27 13:11:53 -0700

    Update

--- Angular/angular-products-project/project/src/app/register/register.component.ts
+++ Angular/angular-products-project/project/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
