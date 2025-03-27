e1022b693125eb14a82a70bd9b11dc59b4213efe
ziad shalaby <shalabyziad94@gmail.com>
2025-03-27 13:02:05 -0700

    Update

--- Angular/angular-products-project/project/src/app/register/register.component.ts
+++ Angular/angular-products-project/project/src/app/register/register.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  public formData: any
+  constructor(private fp:FormBuilder) {
+    this.formData = this.fp.group({
+      "username": ['', Validators.required],
+      "psw": ['', Validators.required],
+      "pswrepeat": ['', Validators.required]
+    })
+  }
+  submitData() {
+    console.log(this.formData.value)
+  }
+  get username() {
+    return this.formData.controls.username
+  }
+  get psw() {
+    return this.formData.controls.psw
+  }
+  get pswrepeat() {
+    return this.formData.controls.pswrepeat
+  }
 }
